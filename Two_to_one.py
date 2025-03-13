0e1b7895df79e7d4e421645c3f92f1d531c7e532
pavlo64 <pavlohontariev@gmail.com>
2025-03-13 11:06:49 +0200

    new tasks

---
+++ Two_to_one.py
@@ -0,0 +1,18 @@
+"""
+Take 2 strings s1 and s2 including only letters from a to z.
+ Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+"""
+
+def longest(a1, a2):
+    union = set(a1).union(set(a2))
+    sorted_text = "".join(sorted(union))
+    return sorted_text
