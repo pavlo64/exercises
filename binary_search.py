c5ec0670b48391e5935402b2b556bc8120ef6c4d
pavlo64 <pavlohontariev@gmail.com>
2025-03-13 13:02:27 +0200

    binary_search

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+my_list = [1,3,5,6,7,89,9,45,346,45,64,87,4,7468,35,457,786,0,967,7654,45]
+
+my_list = sorted(my_list)
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = int(low + high)//2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid -1
+        else:
+            low = mid +1
+    return None
+
+item = 35
+
+print (binary_search(my_list, item))
