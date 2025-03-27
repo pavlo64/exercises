1ff19b11c98ccbf49b9b7cd328638d12e04fca8a
pavlo64 <pavlohontariev@gmail.com>
2025-03-27 18:19:01 +0200

    adeed new id's and sportsbook bet history

--- sportsbook_class.py
+++ sportsbook_class.py
@@ -1,24 +1,43 @@
 import random
 from decimal import Decimal, ROUND_HALF_UP
-from binary_search import binary_search
+import uuid
+import binary_search
+
 class Sportsbook:
     place_bets = True
-    def __init__(self, name, url):
+
+    def __init__(self, name, url, bet_history=None):
+        if bet_history is None:
+            bet_history: dict = {}
         self.name = name
         self.url = url
-    def print_name(self):
-        return f"You can place bet in {self.name} sportsbook"
-    
-betby = Sportsbook('Betby', 'betby.com')
+        self.bet_history = bet_history
+
+    def __repr__(self):
+        return f"{self.name}, Bet History={self.bet_history}"
+
+class Bet:
+
+    def __init__(self, amount: Decimal, k: Decimal) -> None:
+        self.amount = amount
+        self.k = k
+        self.is_settled = None
+        self.id = uuid.uuid4()
+
+    def __repr__(self):
+        return f"ID: {self.id}, Amount: {self.amount}, Odds: {self.k}, Settlement: {self.is_settled}"
+
 
 class Player(Sportsbook):
-    def __init__(self, name, balance, bet_history=None):
+    def __init__(self, name, balance, sportsbook: Sportsbook, bet_history=None):
         if bet_history is None:
-            bet_history = []
+            bet_history: list[Bet] = []
         self.name = name
         self.balance = balance
         self.bet_history = bet_history
-        self.bet_id = 0
+        self.id = uuid.uuid4()
+        sportsbook.bet_history[self.id] = self.bet_history
+
     def place_bet(self):
         if self.place_bets:
             while True:
@@ -29,7 +48,8 @@ class Player(Sportsbook):
                         print("Amount must be greater than 0")
                         continue
                     # Ensure max 2 decimal places
-                    amount = amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+                    amount = amount.quantize(Decimal('0.01'),
+                                             rounding=ROUND_HALF_UP)
                     if amount > self.balance:
                         print("Not enough money to place bet")
                         return
@@ -39,34 +59,65 @@ class Player(Sportsbook):
             k = Decimal(round(random.uniform(1, 10), 2))
             k = k.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
 
-            self.bet_id += 1
             self.balance -= amount
-            new_bet = [self.bet_id, amount, k]
+            new_bet = Bet(amount=amount, k=k)
+            # new_bet.id += 1  # Increment bet_id for next bet
             self.bet_history.append(new_bet)
 
-            print(f"Bet placed: ID={self.bet_id}, Amount={amount}, Odds={k} \nBalance={self.balance}")
+            print(
+                f"Bet placed: ID={new_bet.id}, Amount={amount}, Odds={k} \nBalance={self.balance}")
         else:
             print("Betting is not available now.")
 
     def mybets(self):
-        print (self.bet_history)
+        print(self.bet_history)
+
     def bet_settelment(self):
         bet_id = input('Enter bet id: ')
-        bet_is_won = random.choice([True,False])
+        bet_is_won = random.choice([True, False])
         if int(bet_id) > len(self.bet_history):
             print("Bet with this ID is not exist")
             return
-        element = binary_search(self.bet_history, bet_id)
-        self.bet_history[element].append(bet_is_won)
+        # todo: refactor this
+        bet = None
+        for b in self.bet_history:
+            if b.id == int(bet_id):
+                bet = b
+                break
+
+        bet.is_settled = bet_is_won
         if bet_is_won:
-            self.balance += self.bet_history[element][1] * self.bet_history[element][2]
-            print(f"Bet is won: ID={self.bet_history[element][0]}, Amount={self.bet_history[element][1]}, Odds={self.bet_history[element][2]} \n Balance={self.balance}")
+            self.balance += bet.amount * bet.k
+            print(
+                f"Bet is won: {bet} \n Balance={self.balance}")
         else:
-            print(f"Bet is lost: ID={self.bet_history[element][0]}, Amount={self.bet_history[element][1]}, Odds={self.bet_history[element][2]} \n Balance={self.balance}")
+            print(
+                f"Bet is lost: {bet} \n Balance={self.balance}")
+
 
 if __name__ == '__main__':
-    player = Player("John", Decimal('100.00'))
-    player.place_bet()
-    player.mybets()
-    player.bet_settelment()
-    player.mybets()
+    betby = Sportsbook('Betby', 'betby.com')
+
+    player1 = Player("John", Decimal('100.00'), betby)
+    player2 = Player("Scott", Decimal('300.00'), betby)
+    print(betby)
+
+    player1.place_bet()
+    player2.place_bet()
+    player2.place_bet()
+    player1.place_bet()
+    player1.place_bet()
+    player1.bet_settelment()
+    player2.bet_settelment()
+    print(player1.balance)
+    print(betby)
+
+
+
+
+
+
+
+
+
+
